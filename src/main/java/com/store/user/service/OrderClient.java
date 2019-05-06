8b633e523cbe57be9c4ffbd7527e77d183433f22
Jithin Pariyarath <jithin.pariyarath@coda.global>
2019-05-06 13:42:49 +0530

    Changed Feign Client URL

--- src/main/java/com/store/user/service/OrderClient.java
+++ src/main/java/com/store/user/service/OrderClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 
-@FeignClient(name = "Order")
+@FeignClient(name = "Order",url = "${server.orderUrl}")
 public interface OrderClient {
     @RequestMapping(method = RequestMethod.GET, value = "api/orders/user/{id}")
     Long getOrders(@PathVariable(value = "id") Long id);
